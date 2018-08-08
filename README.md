a26fbe338c322611c96e5c48fc06339c03fc6b0f
Ryoma <xiajianming@quqi.com>
2018-08-08 21:12:13 +0800

    chapter 01 test

---
+++ README.md
@@ -0,0 +1,14 @@
+Redis in Action (NodeJS)
+
+## Install
+```
+yarn install
+```
+
+## Run Test
+```
+npm run test
+```
+
+## Test Background
+- Windows 10 and node v9.6.1
---
+++ ch01/main.js
@@ -0,0 +1,113 @@
+const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
+const VOTE_SCORE = 432;
+const ARTICLES_PER_PAGE = 25;
+
+const current_timestamp = () => {
+    return Math.floor(Date.now() / 1000);
+}
+
+const articleVote = async function (client, user, article) {
+    try {
+        const cutoff = current_timestamp() - ONE_WEEK_IN_SECONDS;
+
+        if (await client.zscore('time:', article) < cutoff) {
+            return;
+        }
+
+        const articleId = article.substring(article.indexOf(':') + 1);
+        if (await client.sadd(`voted:${articleId}`, user)) {
+            await client.zincrby('score:', VOTE_SCORE, article);
+            await client.hincrby(article, 'votes', 1);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const postArticle = async function (client, user, title, link) {
+    try {
+        const articleId = await client.incr('article:');
+
+        const voted = `voted:${articleId}`;
+        await client.sadd(voted, user);
+        await client.expire(voted, ONE_WEEK_IN_SECONDS);
+
+        const article = `article:${articleId}`;
+        await client.hmset(article, {
+            title,
+            link,
+            'poster': user,
+            'time': current_timestamp(),
+            'votes': 1
+        });
+
+        await client.zadd('score:', current_timestamp() + VOTE_SCORE, article);
+        await client.zadd('time:', current_timestamp(), article);
+
+        return articleId;
+    } catch (err) {
+        console.log(err);
+        return 0;
+    }
+};
+
+const getArticles = async function (client, page, order = 'score:') {
+    try {
+        const start = (page - 1) * ARTICLES_PER_PAGE;
+        const end = start + ARTICLES_PER_PAGE - 1;
+
+        const ids = await client.zrevrange(order, start, end);
+
+        const articles = [];
+        for (let id of ids) {
+            const article_data = await client.hgetall(id);
+            article_data[id] = id;
+            articles.push(article_data);
+        }
+
+        return articles;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+};
+
+const addRemoveGroups = async function (client, articleId, toAdd = [], toRemove = []) {
+    try {
+        const article = `article:${articleId}`;
+
+        for(let group of toAdd) {
+            await client.sadd(`group:${group}`, article);
+        }
+        for(let group of toRemove) {
+            await client.srem(`group:${group}`, article);
+        }
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+};
+
+const getGroupArticles = async function (client, group, page, order='score:') {
+    try {
+        const key = `${order}${group}`;
+
+        if(await client.exists(key) === 0) {
+            await client.zinterstore(key, 2, `group:${group}`, order, 'AGGREGATE', 'max');
+            await client.expire(key, 60);
+        }
+
+        return await getArticles(client, page, key);
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+};
+
+module.exports = {
+    articleVote,
+    postArticle,
+    getArticles,
+    addRemoveGroups,
+    getGroupArticles,
+};
